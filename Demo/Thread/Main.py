bca2b26e057f08684878dc468c66aa6a343b4baa
JQ <hellocatmao@gmail.com>
2017-10-12 13:34:07 +0800

    多线程

--- Demo/Thread/Main.py
+++ Demo/Thread/Main.py
@@ -20,12 +20,35 @@ def loop():
 
     while n < 5:
         n = n + 1
-        print('thread %s >> %s')
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
 
-    pass
+    print('thread %s ended,' %threading.current_thread().name)
+    '''
+    
+    current_thread():
+它永远返回当前线程的实例，主线程的实例名字叫MainThread，子线程的名字在创建时指定。名字仅仅用来打印时用来显示，
+如果不起名字Python就会自动给线程命名Thread-1，Thread-2.... 
+    
+    '''
+
+def Demo():
+
+    '''
+
+    多进程和多线程最大的不同在于，多进程中，同一个变量，各自有一份拷贝存在于每个进程中，互不影响。
+    而多线程中
+
+    :return:
+    '''
+
 
 def Main():
-    pass
+    print('thread %s is running...' % threading.current_thread().name)
+    t = threading.Thread(target=loop,name='LookThread')
+    t.start()
+    t.join()
+    print('thread %s ended.' % threading.current_thread().name)
 
 if __name__ == '__main__':
-    pass
+    Main()
