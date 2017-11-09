bf7021225dd6e17b1d443f1398d1f025d3c13ea3
JQ <hellocatmao@gmail.com>
2017-11-09 23:49:01 +0800

    Demo

--- Demo/Pillow_Demo.py
+++ Demo/Pillow_Demo.py
@@ -55,7 +55,30 @@ def Geometric_Transformation():
 
     几何变换
 
+    Image类有resize()、rotate()、和transpose()、transform()方法进行几何变换
+
     :return:
+    '''
+
+    out = image.resize((128, 128))  #调整图像大小
+
+    out = image.rotate(45)  #顺时针旋转45度
+
+
+    out = image.transpose(Image.FLIP_LEFT_RIGHT)    #左右翻转
+    out = image.transpose(Image.FLIP_TOP_BOTTOM)    #顶部底部翻转
+    out = image.transponse(Image.ROTATE_90)
+    out = image.transponse(Image.ROTATE_180)
+    out = image.transponse(Image.ROTATE_270)
+
+
+    '''
+    
+    transponse()和rotate没有性能差别
+    
+    更通用的图像变换方法可以使用transform()
+    
+    
     '''
 
     pass
@@ -70,6 +93,24 @@ def Color_Conversion():
 
     :return:
     '''
+
+
+
+    pass
+
+
+def Schema_Transformation():
+
+    '''
+
+    模式转换
+
+    利用Image类中的convert方法
+
+    :return:
+    '''
+
+    out = image.convert('L')
 
     pass
 
@@ -87,9 +128,39 @@ def Image_Demo():
     pass
 
 
+def Image_Enhancement():
+
+    '''
+
+    图像增强、像素点处理
+
+    ImageFilter模块包含了很多预定义的增强filters，通过filter()方法使用
+
+    :return:
+    '''
+
+    out = image.filter(ImageFilter.DETAIL)
+
+
+    '''
+    像素点处理
+    point()方法通过一个函数或者查询表对图像中的像素点进行处理(例如对比度操作)
+    '''
+
+    out = image.point(lambda i: i * 1.2)    #把每个像素都乘以1.2
+
+    #上面的point()方法是对图像进行处理，还可以组合point()和paste()选择性地处理图片的某一区域
 
 
 
+    #处理单独通道
+    source = image.split()
+
+    R, G, B = 0, 1, 2
+
+
+
+    pass
 def Main():
     Image_Demo()
 
---
+++ SecTools/Windows_RAT/GitHub_RAT.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/25 下午1:17
+# @Author  : 逍遥哥哥
+# @Email   : hellocatmao@gmail.com
+# @File    : GitHub_RAT.py
+# @Software: PyCharm
+
+'''
+
+
+基于GitHub的命令和控制
+
+
+'''
+
